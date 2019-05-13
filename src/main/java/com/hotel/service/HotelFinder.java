5d9e0e9bb3390171eced4d0a5ee32a0d640e62e2
Praveer Satyam <praveer.satyam@INpraveersatyam.local>
2019-05-13 15:21:27 +0530

    [Praveer] adds functions to find cost for rewards and regular customer

--- src/main/java/com/hotel/service/HotelFinder.java
+++ src/main/java/com/hotel/service/HotelFinder.java
@@ -1,52 +1,93 @@
 package com.hotel.service;
 
-import com.hotel.constants.Constants;
 import com.hotel.entity.Hotel;
 import com.hotel.enums.DayOfWeekEnum;
 
 import java.util.List;
 
+import static com.hotel.constants.Constants.REGULAR_CUSTOMER;
+
 public class HotelFinder {
+    public static Double totalCost, maxTotalCostValueTillNow;
+    public static Integer bestRatingTillNow;
+    public static String bestHotelName;
+
+
     public String findBestHotelAvailableByDate(List<Hotel> hotelList, List<DayOfWeekEnum> dayOfWeekList, String customerType) {
-        Double cost = 0.0, resultSum = Double.MAX_VALUE;
-        Integer resultRating = Integer.MAX_VALUE;
-        String resultHotelName = "";
+        totalCost = 0.0;
+        maxTotalCostValueTillNow = Double.MAX_VALUE;
+        bestRatingTillNow = Integer.MIN_VALUE;
+        bestHotelName = "";
         for (Hotel hotel : hotelList) {
-            cost = findCostForHotelByDayList(dayOfWeekList, customerType, hotel);
-            if (resultSum >= cost) {
-                if (resultSum == cost) {
-                    if (hotel.getRating() < resultRating) {
-                        resultHotelName = hotel.getName();
-                        resultRating = hotel.getRating();
+            totalCost = findCostForHotelByDayList(dayOfWeekList, customerType, hotel);
+            if (isMaxAmountTillNowGreaterThanCurrentAmount(totalCost, maxTotalCostValueTillNow)) {
+                if (isMaxAmountTillNowEqualToCurrentAmount(totalCost, maxTotalCostValueTillNow)) {
+                    if (isCurrentRatingGreater(bestRatingTillNow, hotel)) {
+                        updateCheapestHotelCostAndRating(hotel);
                     }
                 } else {
-                    resultHotelName = hotel.getName();
-                    resultSum = cost;
-                    resultRating = hotel.getRating();
+                    updateCheapestHotelCostAndRating(hotel);
                 }
             }
 
         }
-        return resultHotelName;
+        return bestHotelName;
+    }
+
+    private void updateCheapestHotelCostAndRating(Hotel hotel) {
+        bestHotelName = hotel.getName();
+        maxTotalCostValueTillNow = totalCost;
+        bestRatingTillNow = hotel.getRating();
+    }
+
+    private boolean isCurrentRatingGreater(Integer bestRatingTillNow, Hotel hotel) {
+        return hotel.getRating() > bestRatingTillNow;
+    }
+
+    private boolean isMaxAmountTillNowEqualToCurrentAmount(Double totalCost, Double maxTotalCostValueTillNow) {
+        return maxTotalCostValueTillNow == totalCost;
+    }
+
+    private boolean isMaxAmountTillNowGreaterThanCurrentAmount(Double totalCost, Double maxTotalCostValueTillNow) {
+        return maxTotalCostValueTillNow >= totalCost;
     }
 
     private Double findCostForHotelByDayList(List<DayOfWeekEnum> dayOfWeekList, String customerType, Hotel hotel) {
-            Double cost=0.0;
+        Double cost=0.0;
         for (DayOfWeekEnum dayOfWeek : dayOfWeekList) {
-            if (customerType.equals(Constants.REGULAR_CUSTOMER)) {
-                if (DayOfWeekEnum.SATURDAY == dayOfWeek || DayOfWeekEnum.SUNDAY == dayOfWeek) {
-                    cost += hotel.getRates().getRegularWeekend();
-                } else {
-                    cost += hotel.getRates().getRegularWeekDays();
-                }
-            } else {
-                if (DayOfWeekEnum.SATURDAY == dayOfWeek || DayOfWeekEnum.SUNDAY == dayOfWeek) {
-                    cost += hotel.getRates().getRewardsWeekend();
-                } else {
-                    cost += hotel.getRates().getRewardsWeekDays();
-                }
-            }
+            cost = addRatesForCustomer(customerType, hotel, cost, dayOfWeek);
         }
         return cost;
+    }
+
+    private Double addRatesForCustomer(String customerType, Hotel hotel, Double cost, DayOfWeekEnum dayOfWeek) {
+        if (customerType.equals(REGULAR_CUSTOMER)) {
+            cost = addRatesForRegularCustomer(hotel, cost, dayOfWeek);
+        } else {
+            cost = addRatesForRewardsCustomer(hotel, cost, dayOfWeek);
+        }
+        return cost;
+    }
+
+    private Double addRatesForRewardsCustomer(Hotel hotel, Double cost, DayOfWeekEnum dayOfWeek) {
+        if (isWeekend(dayOfWeek)) {
+            cost += hotel.getRates().getRewardsWeekend();
+        } else {
+            cost += hotel.getRates().getRewardsWeekDays();
+        }
+        return cost;
+    }
+
+    private Double addRatesForRegularCustomer(Hotel hotel, Double cost, DayOfWeekEnum dayOfWeek) {
+        if (isWeekend(dayOfWeek)) {
+            cost += hotel.getRates().getRegularWeekend();
+        } else {
+            cost += hotel.getRates().getRegularWeekDays();
+        }
+        return cost;
+    }
+
+    private boolean isWeekend(DayOfWeekEnum dayOfWeek) {
+        return DayOfWeekEnum.SATURDAY == dayOfWeek || DayOfWeekEnum.SUNDAY == dayOfWeek;
     }
 }
